064fd9d5611bd914ae821b8655d6d092ec8f4814
Rafael Laurentino Viana <rafa.lviana@gmail.com>
2021-09-23 22:36:00 -0300

    inicio C#

---
+++ .NET ( C# )/DigitalInnovationOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numDeVezes = 5;
+
+            for (int i = 0; i < numDeVezes; i++)
+            {
+                Console.WriteLine( $"O numero de vezes é : {i}");
+            }
+        }
+    }
+}
